2357bc76dcf965a2553ca7ce046bf336aeb376cf
Sarah Michelle Ang <75544414+yuzuryuu@users.noreply.github.com>
2024-03-14 13:38:47 -0400

    no arrows but with pages

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const splide = new Splide('.splide', {
-    type: 'slide', //slide, loop, fade
-    padding: { y: 10 }, //css values
-    gap: '20px', //1 rem default
+    type: 'loop', //slide, loop, fade
+    // padding: { y: 10 }, //css values
+    // gap: '20px', //1 rem default
     // rewind: true,
     speed: 500, //transition in milliseconds
-    // fixedWidth: '10vw',
-    // width: '100vw',
-    perPage: 5,
-    start: 3,
-    perMove: 2,
+    // fixedWidth: '100vw',
+    // width: '200vw',
+    perPage: 3,
+    // start: 5,
+    perMove: 1,
     autoplay: true,
     interval: 3000,
     arrows: false,
-    pagination: false,
+    // pagination: false,
     pauseOnHover: true,
     wheel: true,
   });
   splide.mount();
-  
